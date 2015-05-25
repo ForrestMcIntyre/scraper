6371ba22f8c9da105c2060415bca2f5b627fea01
Forrest McIntyre <fmcint02@seattlecentral.edu>
2015-05-24 20:05:07 -0700

    add scraper route, html, and client code

--- server/routes.js
+++ server/routes.js
@@ -1,18 +1,23 @@
 var routes = [
+{
+	method: "GET",
+	path:"/assets/{param*}",
+		handler: {
+		directory:{
+			path: "build"
+		}
+	}
+},
 	{
-	  method: "GET",
-	  path:"/assets/{param*}",
-	  handler: {
-	    directory:{
-	      path: "build"
-	    }
-	  }
-	},
-	{
-    method: "GET",
-    path:"/",
-    handler:require("./handlers/welcome")
-  }
+	method: "GET",
+	path:"/",
+	handler:require("./handlers/welcome")
+},
+{
+	method: "GET",
+	path: "/scraper",
+	handler:require("./handlers/scraper")
+}
 ];
 
 module.exports = routes;
