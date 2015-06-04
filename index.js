1ae2b8fb73e5f0758a2dad83c7e1acd8e59cf608
Forrest McIntyre <fmcint02@seattlecentral.edu>
2015-06-04 11:05:41 -0700

    adds proxy for iFrame

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var hapi = require("hapi");
-var server = new hapi.Server();
+var request = require("request");
+var through = require("through2");
 
+var server = new hapi.Server();
 server.connection({port:8000});
 server.start();
 
@@ -15,3 +17,14 @@ server.views({
 });
 
 server.route(require("./server/routes.js"));
+
+server.route({
+   method: "GET",
+   path: "/proxy/{url}",
+   handler: function(req, reply) {
+      var url = "http://" + req.params.url;
+      var stream = through();
+      request(url).pipe(stream);
+      reply(stream);
+   }
+});
